f2f75181e1051777d55799a35a35905b20cffb48
Ayoub Najieddine <ayoub.najieddine@gmail.com>
2017-05-16 19:30:21 +0100

    almost ending info box

--- resources/views/retail/info.blade.php
+++ resources/views/retail/info.blade.php
@@ -1,12 +1,25 @@
 @extends("layouts.master")
 
 @section("content")
-<link type="text/css" href="{{ URL::to('css/search.css') }}" rel="stylesheet">
-<script src="{{ URL::to('js/search.js') }}" type="text/javascript"></script>
+<link type="text/css" href="{{ URL::to('css/info.css') }}" rel="stylesheet">
+<script src="{{ URL::to('js/info.js') }}" type="text/javascript"></script>
 		<div id="infoBoxDiv">
-			<div id="SideLeft">
+			<div id="headRet">
+			<h2 class="pull-right">{{ $infos->adresse_retail }}</h2>
+			<div class="pull-left">
+			<button class="btn btn-primary"><span class="glyphicon glyphicon-share"></span> Partager</button>	
+			<button class="btn btn-link"><span class="glyphicon glyphicon-print"></span> Imprimer</button>
+			<button class="btn btn-link"><span class="glyphicon glyphicon-pushpin"></span> Save</button>
+			</div>
+			</div>
+			<hr />
+			<div id="SideLeft" class="pull-left">
 			<div id="imgList">
+				<center>
+				<!-- span class="glyphicon glyphicon-menu-right"></span -->
 				<img id="mainImg" src="/thumbs/{{ $pics[0]->picture_name }}">
+				<!-- span class="glyphicon glyphicon-menu-left"></span -->
+				</center>
 				@if($pics->count() > 0)
 					<div id="PicsListSel">
 					@foreach($pics as $pic)
@@ -18,8 +31,8 @@
 				@endif
 			</div>		
 				<div id="partmaDetail">
-		<table class="table panel panel-default">
-		
+				
+		<table class="table panel panel-default pull-right" id="mainInfo" dir="rtl">
 			<tr><th>{{ trans("new.city") }}</th><td>{{ trans("cities.".$infos->city_id) }}</td></tr>
 			<tr><td colspan="2" ><b>{{ trans("dashboard.retail")  }} {{trans("dashboard.".$infos->rent."_rt") }}</b></td></tr>
 			<tr><th>{{ trans("new.price") }}</th><td>{{ $infos->price }}</td></tr>
@@ -27,22 +40,28 @@
 			<tr><th>{{ trans("new.type") }}</th><td>{{ trans("new.".$infos->type) }}</td></tr>
 			<tr><th>{{ trans("new.address") }}</th><td>{{ $infos->adresse_retail }}</td></tr>
 		</table>
+				<div class="pull-left panel panel-default" id="addInfo">
+				<div class="panel-body">
+					{{ $infos->info }}
 				</div>
-
+				</div>
+				</div>
 			</div>
-			<div id="SideRight">
+			<div id="SideRight" class="pull-right">
+
 				<div id="cntDetail">
 					<div class="panel panel-default">
 					<div class="panel-heading">Contact Info</div>
-					<div class="panel-body">	
-					<button class="btn btn-md btn-success fl ">Call Number</button>
-					<button class="btn btn-md btn-link fl">Send Message</button>
+					<div class="panel-body">
+					<h2 align="center" style="color:#006cc9;"><strong>{{ $infos->price }} DH</strong></h2>	
+					<button class="btn btn-lg btn-success fl" style="margin-bottom:10px;">Call Number</button>
+					<button class="btn btn-lg btn-link fl">Send Message</button>
 					</div>
 					</div>
 				</div>
 				<div id="userDetail">
 					<div class="panel panel-default">
-						<div class="panel-heading"><strong><span class="glyphicon glyphicon-user"></span><a style="color:#333;" href="/viewusr/{{ $userInfo->id }}"><strong> {{ $userInfo->full_name}}</strong></a><span id="starts"><strong></strong></span></div>
+						<div class="panel-heading"><span class="glyphicon glyphicon-user"></span><a style="color:#333;" href="/viewusr/{{ $userInfo->id }}"><strong> {{ $userInfo->full_name}}</strong></a><span id="starts"><strong></strong></span></div>
 								<?php  $count = $userInfo->retails->count(); ?>
 						<!-- 
 					Link to Profile of the user;
@@ -51,7 +70,7 @@
 						<div class="panel-body">
 							@if($count > 0)
 								Number Of retails are {{ $count }}<br>
-								<a>see other retails for the user</a>
+								<a>user other retails</a><br>
 							@endif
 							date Of post {{ $infos->created_at}}
 							
@@ -62,3 +81,5 @@
 			</div>
 		</div>	
 @endsection
+
+
